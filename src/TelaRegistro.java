02bca32bec88cd910aa7b29761c860c64bcd6404
marianna lima <140180165+Limaniteroi@users.noreply.github.com>
2025-01-29 00:25:33 -0300

    cores e botoes

--- src/TelaRegistro.java
+++ src/TelaRegistro.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+
 public class TelaRegistro extends JFrame {
     private Sistema sistema;
     private Usuario novoUsuario;
@@ -16,13 +17,14 @@ public class TelaRegistro extends JFrame {
 
         JPanel mainPanel = new JPanel(new GridLayout(0, 2, 5, 5));
         mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        mainPanel.setBackground(new Color(235,68,71));
 
         // Campos de registro
         JTextField loginField = new JTextField(20);
         JPasswordField senhaField = new JPasswordField(20);
         JTextField nomeField = new JTextField(20);
         JSpinner idadeSpinner = new JSpinner(new SpinnerNumberModel(18, 18, 100, 1));
-        JComboBox<String> sexoCombo = new JComboBox<>(new String[]{"M", "F", "O"});
+        JComboBox<String> sexoCombo = new JComboBox<>(new String[]{"Masculino", "Feminino", "Prefiro não declarar"});
         JTextField cidadeField = new JTextField(20);
         JTextField prefMusicalField = new JTextField(20);
         JCheckBox bebeCheck = new JCheckBox();
@@ -32,7 +34,7 @@ public class TelaRegistro extends JFrame {
         JTextField hobbiesField = new JTextField(20);
         JCheckBox trabalhaCheck = new JCheckBox();
         JCheckBox faculdadeCheck = new JCheckBox();
-        JComboBox<String> periodoCombo = new JComboBox<>(new String[]{"D", "N"});
+        JComboBox<String> periodoCombo = new JComboBox<>(new String[]{"Diurno", "Noturno"});
         JCheckBox exercitaCheck = new JCheckBox();
         JTextArea descricaoArea = new JTextArea(4, 20);
 
@@ -45,16 +47,48 @@ public class TelaRegistro extends JFrame {
         mainPanel.add(nomeField);
         mainPanel.add(new JLabel("Idade:"));
         mainPanel.add(idadeSpinner);
-        // ... adicionar outros campos
+        mainPanel.add(new JLabel("Sexo:"));
+        mainPanel.add(sexoCombo);
+        mainPanel.add(new JLabel("Cidade:"));
+        mainPanel.add(cidadeField);
+        mainPanel.add(new JLabel("Preferência Musical:"));
+        mainPanel.add(prefMusicalField);
+        mainPanel.add(new JLabel("Bebe?"));
+        mainPanel.add(bebeCheck);
+        mainPanel.add(new JLabel("Fuma?"));
+        mainPanel.add(fumaCheck);
+        mainPanel.add(new JLabel("Orientação Sexual:"));
+        mainPanel.add(orientacaoField);
+        mainPanel.add(new JLabel("Hobbies:"));
+        mainPanel.add(hobbiesField);
+        mainPanel.add(new JLabel("Trabalha?"));
+        mainPanel.add(trabalhaCheck);
+        mainPanel.add(new JLabel("Faculdade?"));
+        mainPanel.add(faculdadeCheck);
+        mainPanel.add(new JLabel("Período:"));
+        mainPanel.add(periodoCombo);
+        mainPanel.add(new JLabel("Se exercita?"));
+        mainPanel.add(exercitaCheck);
+        mainPanel.add(new JLabel("Descrição:"));
+        mainPanel.add(new JScrollPane(descricaoArea));
 
         JButton registrarButton = new JButton("Registrar");
         registrarButton.addActionListener(e -> {
             novoUsuario.setLogin(loginField.getText());
             novoUsuario.setSenha(new String(senhaField.getPassword()));
             novoUsuario.setNome(nomeField.getText());
-            novoUsuario.setIdade((Integer)idadeSpinner.getValue());
-            // ... setar outros campos
-
+            novoUsuario.setIdade((Integer) idadeSpinner.getValue());
+            novoUsuario.setCidade(cidadeField.getText());
+            novoUsuario.setPrefMusical(prefMusicalField.getText());
+            novoUsuario.setBebe(bebeCheck.isSelected());
+            novoUsuario.setFuma(fumaCheck.isSelected());
+            novoUsuario.setOrientacaoSexual(orientacaoField.getText());
+            novoUsuario.setHobbies(hobbiesField.getText());
+            novoUsuario.setTrabalha(trabalhaCheck.isSelected());
+            novoUsuario.setFaculdade(faculdadeCheck.isSelected());
+            novoUsuario.setPeriodo(periodoCombo.getSelectedItem().toString().charAt(0));
+            novoUsuario.setExercita(exercitaCheck.isSelected());
+            novoUsuario.setDescricao(descricaoArea.getText());
             novoUsuario.salvarUsuario();
             JOptionPane.showMessageDialog(this, "Usuário registrado com sucesso!");
             new TelaInicial().setVisible(true);

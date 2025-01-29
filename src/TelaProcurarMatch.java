3b430f45f6f909ca834376c7bfdb6a39a811dd12
Júlia Vogel de Moraes <juliavogelm@gmail.com>
2025-01-29 11:03:31 -0300

    Ajustando a forma que os dados são mostrados

--- src/TelaProcurarMatch.java
+++ src/TelaProcurarMatch.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 public class TelaProcurarMatch extends JFrame {
     private Sistema sistema;
     private Usuario usuarioAtual;
+    private Usuario usuarioLogado;
 
     public TelaProcurarMatch(Sistema sistema) {
         this.sistema = sistema;
@@ -23,15 +24,36 @@ public class TelaProcurarMatch extends JFrame {
         JPanel userInfoPanel = new JPanel();
         userInfoPanel.setLayout(new BoxLayout(userInfoPanel, BoxLayout.Y_AXIS));
 
-        JLabel fotoLabel = new JLabel();
         JLabel nomeLabel = new JLabel();
         JLabel idadeLabel = new JLabel();
-        JTextArea descricaoArea = new JTextArea();
+        JLabel sexoLabel = new JLabel();
+        JLabel cidadeLabel = new JLabel();
+        JLabel prefMusicalLabel = new JLabel();
+        JLabel bebeLabel = new JLabel();
+        JLabel fumaLabel = new JLabel();
+        JLabel orientacaoLabel = new JLabel();
+        JLabel hobbiesLabel = new JLabel();
+        JLabel trabalhaLabel = new JLabel();
+        JLabel faculdadeLabel = new JLabel();
+        JLabel periodoLabel = new JLabel();
+        JLabel exercitaLabel = new JLabel();
+        JTextArea descricaoArea = new JTextArea(4, 20);
         descricaoArea.setEditable(false);
 
-        userInfoPanel.add(fotoLabel);
         userInfoPanel.add(nomeLabel);
         userInfoPanel.add(idadeLabel);
+        userInfoPanel.add(sexoLabel);
+        userInfoPanel.add(cidadeLabel);
+        userInfoPanel.add(prefMusicalLabel);
+        userInfoPanel.add(bebeLabel);
+        userInfoPanel.add(fumaLabel);
+        userInfoPanel.add(orientacaoLabel);
+        userInfoPanel.add(hobbiesLabel);
+        userInfoPanel.add(trabalhaLabel);
+        userInfoPanel.add(faculdadeLabel);
+        userInfoPanel.add(periodoLabel);
+        userInfoPanel.add(exercitaLabel);
+        userInfoPanel.add(new JLabel("Descrição:"));
         userInfoPanel.add(new JScrollPane(descricaoArea));
 
         // Botões de ação
@@ -44,14 +66,24 @@ public class TelaProcurarMatch extends JFrame {
         buttonPanel.add(likeButton);
         buttonPanel.add(denunciarButton);
 
-        // Ações dos botões
+        // Ação de mostrar o próximo usuário
         ActionListener proximoUsuario = e -> {
             usuarioAtual = sistema.getRandomUser();
             if (usuarioAtual != null) {
-                nomeLabel.setText(usuarioAtual.getNome());
-                idadeLabel.setText(String.valueOf(usuarioAtual.getIdade()));
+                nomeLabel.setText("Nome: " + usuarioAtual.getNome());
+                idadeLabel.setText("Idade: " + usuarioAtual.getIdade());
+                sexoLabel.setText("Sexo: " + usuarioAtual.getSexo());
+                cidadeLabel.setText("Cidade: " + usuarioAtual.getCidade());
+                prefMusicalLabel.setText("Preferência Musical: " + usuarioAtual.getPrefMusical());
+                bebeLabel.setText("Bebe? " + (usuarioAtual.getBebe() ? "Sim" : "Não"));
+                fumaLabel.setText("Fuma? " + (usuarioAtual.getFuma() ? "Sim" : "Não"));
+                orientacaoLabel.setText("Orientação Sexual: " + usuarioAtual.getOrientacaoSexual());
+                hobbiesLabel.setText("Hobbies: " + usuarioAtual.getHobbies());
+                trabalhaLabel.setText("Trabalha? " + (usuarioAtual.getTrabalha() ? "Sim" : "Não"));
+                faculdadeLabel.setText("Faculdade? " + (usuarioAtual.getFaculdade() ? "Sim" : "Não"));
+                periodoLabel.setText("Período: " + usuarioAtual.getPeriodo());
+                exercitaLabel.setText("Se exercita? " + (usuarioAtual.getExercita() ? "Sim" : "Não"));
                 descricaoArea.setText(usuarioAtual.getDescricao());
-                // Atualizar foto
             } else {
                 nomeLabel.setText("Nenhum usuário disponível");
                 idadeLabel.setText("");
@@ -70,6 +102,7 @@ public class TelaProcurarMatch extends JFrame {
 
         denunciarButton.addActionListener(e -> {
             sistema.denunciar(usuarioAtual);
+            new TelaDenunciar(sistema, usuarioLogado, usuarioAtual).setVisible(true);
             proximoUsuario.actionPerformed(null);
         });
 

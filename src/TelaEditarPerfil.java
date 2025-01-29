78e1e1fe48703bac0f5095db6eb0acfb91e1d2d9
Júlia Vogel de Moraes <juliavogelm@gmail.com>
2025-01-29 00:11:08 -0300

    atualizei a tela editar perfil com todos os dados necessarios

--- src/TelaEditarPerfil.java
+++ src/TelaEditarPerfil.java
@@ -26,24 +26,57 @@ public class TelaEditarPerfil extends JFrame {
         JTextField hobbiesField = new JTextField(usuario.getHobbies(), 20);
         JCheckBox trabalhaCheck = new JCheckBox("", usuario.getTrabalha());
         JCheckBox faculdadeCheck = new JCheckBox("", usuario.getFaculdade());
-        JComboBox<String> periodoCombo = new JComboBox<>(new String[]{"D", "N"});
+        JComboBox<String> periodoCombo = new JComboBox<>(new String[]{"Diurno", "Noturno"});
         periodoCombo.setSelectedItem(String.valueOf(usuario.getPeriodo()));
         JCheckBox exercitaCheck = new JCheckBox("", usuario.getExercita());
         JTextArea descricaoArea = new JTextArea(usuario.getDescricao(), 4, 20);
 
-        // Adicionar campos ao painel
         mainPanel.add(new JLabel("Cidade:"));
         mainPanel.add(cidadeField);
-        // ... adicionar outros campos
+
+        mainPanel.add(new JLabel("Preferência Musical:"));
+        mainPanel.add(prefMusicalField);
+
+        mainPanel.add(new JLabel("Bebe?"));
+        mainPanel.add(bebeCheck);
+
+        mainPanel.add(new JLabel("Fuma?"));
+        mainPanel.add(fumaCheck);
+
+        mainPanel.add(new JLabel("Hobbies:"));
+        mainPanel.add(hobbiesField);
+
+        mainPanel.add(new JLabel("Trabalha?"));
+        mainPanel.add(trabalhaCheck);
+
+        mainPanel.add(new JLabel("Faculdade?"));
+        mainPanel.add(faculdadeCheck);
+
+        mainPanel.add(new JLabel("Período:"));
+        mainPanel.add(periodoCombo);
+
+        mainPanel.add(new JLabel("Se exercita?"));
+        mainPanel.add(exercitaCheck);
+
+        mainPanel.add(new JLabel("Descrição:"));
+        mainPanel.add(new JScrollPane(descricaoArea));
 
         JButton salvarButton = new JButton("Salvar Alterações");
         salvarButton.addActionListener(e -> {
             usuario.setCidade(cidadeField.getText());
             usuario.setPrefMusical(prefMusicalField.getText());
-            // ... atualizar outros campos
+            usuario.setBebe(bebeCheck.isSelected());
+            usuario.setFuma(fumaCheck.isSelected());
+            usuario.setHobbies(hobbiesField.getText());
+            usuario.setTrabalha(trabalhaCheck.isSelected());
+            usuario.setFaculdade(faculdadeCheck.isSelected());
+            usuario.setPeriodo(periodoCombo.getSelectedItem().toString().charAt(0));
+            usuario.setExercita(exercitaCheck.isSelected());
+            usuario.setDescricao(descricaoArea.getText());
 
             sistema.atualizarUsuario(usuario);
             JOptionPane.showMessageDialog(this, "Perfil atualizado com sucesso!");
+            new TelaUsuario(sistema).setVisible(true);
             dispose();
         });
 
